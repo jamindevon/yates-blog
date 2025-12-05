04babfd670d053f570a9b961ed5bfbe6af95757a
JaMin Brown <thesunlandcompany@gmail.com>
2025-12-05 10:20:46 -0500

    fix: Robust HubSpot form loading with useEffect

--- components/HubSpotPlanningForm.tsx
+++ components/HubSpotPlanningForm.tsx
@@ -1,21 +1,26 @@
 "use client";
 
-import Script from "next/script";
+import { useEffect } from "react";
 
 export function HubSpotPlanningForm() {
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://js-na2.hsforms.net/forms/embed/244218599.js";
+        script.defer = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
-        <>
-            <Script
-                src="https://js-na2.hsforms.net/forms/embed/244218599.js"
-                strategy="afterInteractive"
-            />
-            <div
-                className="hs-form-frame"
-                data-region="na2"
-                data-form-id="24e41071-680d-4070-8f28-2ed5860de801"
-                data-portal-id="244218599"
-            >
-            </div>
-        </>
+        <div
+            className="hs-form-frame"
+            data-region="na2"
+            data-form-id="24e41071-680d-4070-8f28-2ed5860de801"
+            data-portal-id="244218599"
+        >
+        </div>
     );
 }
